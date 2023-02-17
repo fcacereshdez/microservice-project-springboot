ac49c75d5e926af7e12c5dd9ad22c20357aa6342
Francisco C. Hernández <fcacereshdez@outlook.com>
2023-02-16 21:57:38 -0600

    service configuration added

--- booking-microservice/src/main/java/com/fcacereshdez/bookingmicroservice/BookingMicroserviceApplication.java
+++ booking-microservice/src/main/java/com/fcacereshdez/bookingmicroservice/BookingMicroserviceApplication.java
@@ -2,8 +2,11 @@ package com.fcacereshdez.bookingmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookingMicroserviceApplication {
 
 	public static void main(String[] args) {
--- product-microservice/src/main/java/com/fcacereshdez/productmicroservice/ProductMicroserviceApplication.java
+++ product-microservice/src/main/java/com/fcacereshdez/productmicroservice/ProductMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.fcacereshdez.productmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.*;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductMicroserviceApplication {
 
 	public static void main(String[] args) {
